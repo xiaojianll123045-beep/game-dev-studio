aee506bc986e1cdda40e06d19711d3237c9da3ae
xiaojianll123045-beep <xjllqw@163.com>
2026-07-03 20:17:10 +0800

    Rename project files to English: GameTycoon.csproj/.sln, update build.bat

--- build.bat
+++ build.bat
@@ -1,41 +1,42 @@
 @echo off
+chcp 65001 >nul
 setlocal
 
 set VER=%~1
 if "%VER%"=="" set VER=0.1
+set ENG=C:\Users\xijil\AppData\Local\Microsoft\WinGet\Packages\GodotEngine.GodotEngine.Mono_Microsoft.Winget.Source_8wekyb3d8bbwe\Godot_v4.7-stable_mono_win64\Godot_v4.7-stable_mono_win64.exe
+set PROJ=%~dp0.
 
 echo === Building GameTycoon v%VER% (naked) ===
+echo.
 
-set ENG=C:\Users\xijil\AppData\Local\Microsoft\WinGet\Packages\GodotEngine.GodotEngine.Mono_Microsoft.Winget.Source_8wekyb3d8bbwe\Godot_v4.7-stable_mono_win64\Godot_v4.7-stable_mono_win64.exe
-
-echo === Compiling C# Release ===
-dotnet build "%~dp0游戏开发商.csproj" --configuration Release
+echo [1/4] Compiling C#
+dotnet build "%PROJ%\GameTycoon.csproj" --configuration Release
 if errorlevel 1 (
-    echo === C# build failed ===
+    echo === C# build FAILED ===
     pause
     exit /b 1
 )
 
-echo === Cleaning build folder ===
+echo [2/4] Cleaning build folder
 if exist "%~dp0build" rmdir /S /Q "%~dp0build"
 mkdir "%~dp0build\project"
 
-echo === Copying game files ===
-xcopy "%~dp0icon.png" "%~dp0build\project\" /Y
-xcopy "%~dp0project.godot" "%~dp0build\project\" /Y
-xcopy /E /I "%~dp0assets" "%~dp0build\project\assets"
-xcopy /E /I "%~dp0locales" "%~dp0build\project\locales"
-xcopy /E /I "%~dp0scripts" "%~dp0build\project\scripts"
-xcopy /E /I "%~dp0tutorial_data" "%~dp0build\project\tutorial_data" 2>nul
+echo [3/4] Copying game files
+xcopy "%PROJ%\icon.png" "%~dp0build\project\" /Y >nul
+xcopy "%PROJ%\project.godot" "%~dp0build\project\" /Y >nul
+for %%D in (assets locales scripts tutorial_data) do (
+    if exist "%PROJ%\%%D" (
+        xcopy /E /I "%PROJ%\%%D" "%~dp0build\project\%%D" >nul
+    )
+)
 
-echo === Copying Godot engine ===
-copy "%ENG%" "%~dp0build\GameTycoon.exe" /Y
-
-echo === Creating launcher ===
+echo [4/4] Copying Godot engine and creating launcher
+copy "%ENG%" "%~dp0build\GameTycoon.exe" /Y >nul
 echo @echo off > "%~dp0build\run.bat"
 echo start "" "%%~dp0GameTycoon.exe" --path "%%~dp0project" --rendering-driver d3d12 >> "%~dp0build\run.bat"
 
+echo.
 echo === Done! build\GameTycoon.exe + project\ v%VER% ===
 echo Double-click build\run.bat to play.
-
 pause
