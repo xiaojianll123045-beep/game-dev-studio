fdb28fbb6d00432bec485c893dced857e7333b9d
xiaojianll123045-beep <xjllqw@163.com>
2026-07-05 19:06:19 +0800

    运行游戏.bat 加入 Native DLL 编译 + export 排除 extensions/

--- 运行游戏.bat
+++ 运行游戏.bat
@@ -4,7 +4,23 @@ setlocal
 
 set ENG=C:\Users\xijil\AppData\Local\Microsoft\WinGet\Packages\GodotEngine.GodotEngine.Mono_Microsoft.Winget.Source_8wekyb3d8bbwe\Godot_v4.7-stable_mono_win64\Godot_v4.7-stable_mono_win64.exe
 
-echo [1/2] Cleaning cache and compiling C#
+echo === Dev Run ===
+echo.
+
+echo [1/3] Compiling sandbox native DLL
+pushd "%~dp0extensions\mod_sandbox_native"
+g++ -shared -o mod_sandbox_hook.dll mod_sandbox_hook.cpp -static -static-libgcc -static-libstdc++ -O2 -Wl,--exclude-all-symbols
+if errorlevel 1 (
+    echo === Native DLL build FAILED (确保已安装 MinGW GCC) ===
+    popd
+    pause
+    exit /b 1
+)
+copy /Y mod_sandbox_hook.dll "%~dp0" >nul
+popd
+echo.
+
+echo [2/3] Cleaning cache and compiling C#
 if exist "%~dp0.godot\mono\temp" rmdir /S /Q "%~dp0.godot\mono\temp"
 dotnet build "%~dp0GameTycoon.csproj"
 if errorlevel 1 (
@@ -14,7 +30,7 @@ if errorlevel 1 (
 )
 
 echo.
-echo [2/2] Launching game (close game window to return here)
+echo [3/3] Launching game (close game window to return here)
 echo.
 "%ENG%" --path "%~dp0." --rendering-driver d3d12
 
